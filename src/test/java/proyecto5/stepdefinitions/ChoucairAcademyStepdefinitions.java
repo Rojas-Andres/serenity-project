856a4d68e853077e9467fe8d035bea2308045cd0
RojasxAndres <afrojaslondono@gmail.com>
2021-03-17 21:04:30 -0500

    Se añade stepdefinitions

---
+++ src/test/java/proyecto5/stepdefinitions/ChoucairAcademyStepdefinitions.java
@@ -0,0 +1,24 @@
+package proyecto5.stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ChoucairAcademyStepdefinitions {
+    @Given("^that brandon$")
+    public void thatBrandon() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+
+    @When("^he search$")
+    public void heSearch() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @Then("^he finds$")
+    public void heFinds() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+}
